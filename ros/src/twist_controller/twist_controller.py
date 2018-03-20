36008d1bbe217fce47ae52dd8902ae3fa86b91e0
anmolsidhu <anmolsidhu@gmail.com>
2018-03-20 19:29:36 -0400

    Add files via upload

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,60 +1,50 @@
 from pid import PID
 from yaw_controller import YawController
 import math
-from lowpass import LowPassFilter
-import time
-import rospy
-
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
-MAX_SPEED = 40.0
+
 
 class Controller(object):
     def __init__(self, *args, **kwargs):
         # TODO: Implement
-        self.throttle_pid = PID(kwargs['throttle_gains'])
-        self.yaw_control = YawController(kwargs['wheel_base'], kwargs['steer_ratio'],
-                                         kwargs['min_speed'], kwargs['max_lat_accel'],
-                                         kwargs['max_steer_angle'],
-                                         kwargs['steering_gains']
-                                         )
-        self.last_t = None
-        self.accel_limit = kwargs['accel_limit']
+
+        self.vehicle_mass = kwargs['vehicle_mass']
+        self.fuel_capacity = kwargs['fuel_capacity']
+        self.brake_deadband = kwargs['brake_deadband']
         self.decel_limit = kwargs['decel_limit']
-        self.filter = LowPassFilter(0.2,0.1)
+        self.accel_limit = kwargs['accel_limit']
+        self.wheel_radius = kwargs['wheel_radius']
+        self.wheel_base = kwargs['wheel_base']
+        self.steer_ratio = kwargs['steer_ratio']
+        self.max_lat_accel = kwargs['max_lat_accel']
+        self.max_steer_angle = kwargs['max_steer_angle']
+        min_speed = 0 # TODO: What is a good value for min speed?
 
-    '''
-    Params:
-    target_v - desired linear velocity
-    target_w - desired angular velocity
-    current_v - current linear velocity
-    dbw_enabled - drive by wire enabled (ignore error in this case)
-    '''
-    def control(self, target_v, target_w, current_v, dbw_enabled):
-        # Get throttle value from controller
-        if self.last_t is None or not dbw_enabled:
-            self.last_t = rospy.get_time()
-            return 0.0, 0.0, 0.0
 
-        dt = rospy.get_time() - self.last_t
+        self.linear_pid = PID(kp=0.8, ki=0, kd=0.05, mn=self.decel_limit, mx=0.5 * self.accel_limit)
+        self.yaw_controller = YawController(self.wheel_base, self.steer_ratio, min_speed, self.max_lat_accel, self.max_steer_angle)
+        self.steering_pid = PID(kp=0.15, ki=0.001, kd=0.1, mn=-self.max_steer_angle, mx=self.max_steer_angle)
 
-        error_v = min(target_v.x, MAX_SPEED*ONE_MPH) - current_v.x
-        error_v = max(self.decel_limit*dt, min(self.accel_limit*dt, error_v))
-        throttle = self.throttle_pid.step(error_v, dt)
-        throttle = max(0.0, min(1.0, throttle))
-        if error_v < 0:
-            brake = -15.0*error_v   # Proportional braking
-            brake = max(brake, 1.0)
-            throttle = 0.0
-        else:
-            brake = 0.0
+    def reset(self):
+        self.linear_pid.reset()
+        self.steering_pid.reset()
 
-        # # Special case for stopping
-        if abs(target_v.x) < 0.1:
-            brake = 12.0
+    def control(self, proposed_linear_velocity, proposed_angular_velocity, current_linear_velocity, duration_in_seconds):
+        linear_velocity_error = proposed_linear_velocity - current_linear_velocity
 
-        steer = self.yaw_control.get_steering(target_v.x, target_w.z, current_v.x)
-        steer = self.filter.filt(steer)
-        self.last_t = time.time()
-		steer = 0.0
-        return throttle, brake, steer
+        velocity_correction = self.linear_pid.step(linear_velocity_error, duration_in_seconds)
+
+        brake = 0
+        throttle = velocity_correction
+
+        if(throttle < 0):
+            deceleration = abs(throttle)
+            brake = (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY) * self.wheel_radius * deceleration if deceleration > self.brake_deadband else 0.
+            throttle = 0
+
+        predictive_steering = self.yaw_controller.get_steering(proposed_linear_velocity, proposed_angular_velocity, current_linear_velocity)
+        
+        steering = predictive_steering
+
+        return throttle, brake, steering
